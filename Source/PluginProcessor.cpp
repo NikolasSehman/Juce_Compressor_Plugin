b532f4aa32f39a034209e9bc2947457e659b77e0
Nikolas Sehman <nikolas.sehman@gmail.com>
2021-03-04 05:05:10 -0600

    First commit

---
+++ Source/PluginProcessor.cpp
@@ -0,0 +1,198 @@
+/*
+  ==============================================================================
+
+    This file contains the basic framework code for a JUCE plugin processor.
+
+  ==============================================================================
+*/
+
+#include "PluginProcessor.h"
+#include "PluginEditor.h"
+
+//==============================================================================
+FirstTestPluginAudioProcessor::FirstTestPluginAudioProcessor()
+#ifndef JucePlugin_PreferredChannelConfigurations
+     : AudioProcessor (BusesProperties()
+                     #if ! JucePlugin_IsMidiEffect
+                      #if ! JucePlugin_IsSynth
+                       .withInput  ("Input",  juce::AudioChannelSet::stereo(), true)
+                      #endif
+                       .withOutput ("Output", juce::AudioChannelSet::stereo(), true)
+                     #endif
+                       )
+#endif
+{
+}
+
+FirstTestPluginAudioProcessor::~FirstTestPluginAudioProcessor()
+{
+}
+
+//==============================================================================
+const juce::String FirstTestPluginAudioProcessor::getName() const
+{
+    return JucePlugin_Name;
+}
+
+bool FirstTestPluginAudioProcessor::acceptsMidi() const
+{
+   #if JucePlugin_WantsMidiInput
+    return true;
+   #else
+    return false;
+   #endif
+}
+
+bool FirstTestPluginAudioProcessor::producesMidi() const
+{
+   #if JucePlugin_ProducesMidiOutput
+    return true;
+   #else
+    return false;
+   #endif
+}
+
+bool FirstTestPluginAudioProcessor::isMidiEffect() const
+{
+   #if JucePlugin_IsMidiEffect
+    return true;
+   #else
+    return false;
+   #endif
+}
+
+double FirstTestPluginAudioProcessor::getTailLengthSeconds() const
+{
+    return 0.0;
+}
+
+int FirstTestPluginAudioProcessor::getNumPrograms()
+{
+    return 1;   // NB: some hosts don't cope very well if you tell them there are 0 programs,
+                // so this should be at least 1, even if you're not really implementing programs.
+}
+
+int FirstTestPluginAudioProcessor::getCurrentProgram()
+{
+    return 0;
+}
+
+void FirstTestPluginAudioProcessor::setCurrentProgram (int index)
+{
+}
+
+const juce::String FirstTestPluginAudioProcessor::getProgramName (int index)
+{
+    return {};
+}
+
+void FirstTestPluginAudioProcessor::changeProgramName (int index, const juce::String& newName)
+{
+}
+
+//==============================================================================
+void FirstTestPluginAudioProcessor::prepareToPlay (double sampleRate, int samplesPerBlock)
+{
+    // Use this method as the place to do any pre-playback
+    // initialisation that you need..
+}
+
+void FirstTestPluginAudioProcessor::releaseResources()
+{
+    // When playback stops, you can use this as an opportunity to free up any
+    // spare memory, etc.
+}
+
+#ifndef JucePlugin_PreferredChannelConfigurations
+bool FirstTestPluginAudioProcessor::isBusesLayoutSupported (const BusesLayout& layouts) const
+{
+  #if JucePlugin_IsMidiEffect
+    juce::ignoreUnused (layouts);
+    return true;
+  #else
+    // This is the place where you check if the layout is supported.
+    // In this template code we only support mono or stereo.
+    // Some plugin hosts, such as certain GarageBand versions, will only
+    // load plugins that support stereo bus layouts.
+    if (layouts.getMainOutputChannelSet() != juce::AudioChannelSet::mono()
+     && layouts.getMainOutputChannelSet() != juce::AudioChannelSet::stereo())
+        return false;
+
+    // This checks if the input layout matches the output layout
+   #if ! JucePlugin_IsSynth
+    if (layouts.getMainOutputChannelSet() != layouts.getMainInputChannelSet())
+        return false;
+   #endif
+
+    return true;
+  #endif
+}
+#endif
+
+void FirstTestPluginAudioProcessor::processBlock (juce::AudioBuffer<float>& buffer, juce::MidiBuffer& midiMessages)
+{
+    juce::ScopedNoDenormals noDenormals;
+    auto totalNumInputChannels  = getTotalNumInputChannels();
+    auto totalNumOutputChannels = getTotalNumOutputChannels();
+
+    for (auto i = totalNumInputChannels; i < totalNumOutputChannels; ++i)
+        buffer.clear (i, 0, buffer.getNumSamples());
+
+
+    for (int channel = 0; channel < totalNumInputChannels; ++channel)
+    {
+        for (int n = 0; n < buffer.getNumSamples(); n++){
+            float x = buffer.getReadPointer(channel)[n];
+            //
+            x = hardClip(x);
+            buffer.getWritePointer(channel)[n] = x;
+        }
+
+        // ..do something to the data...
+    }
+}
+
+//==============================================================================
+bool FirstTestPluginAudioProcessor::hasEditor() const
+{
+    return true; // (change this to false if you choose to not supply an editor)
+}
+
+juce::AudioProcessorEditor* FirstTestPluginAudioProcessor::createEditor()
+{
+    return new FirstTestPluginAudioProcessorEditor (*this);
+}
+
+//==============================================================================
+void FirstTestPluginAudioProcessor::getStateInformation (juce::MemoryBlock& destData)
+{
+    // You should use this method to store your parameters in the memory block.
+    // You could do that either as raw data, or use the XML or ValueTree classes
+    // as intermediaries to make it easy to save and load complex data.
+}
+
+void FirstTestPluginAudioProcessor::setStateInformation (const void* data, int sizeInBytes)
+{
+    // You should use this method to restore your parameters from this memory block,
+    // whose contents will have been created by the getStateInformation() call.
+}
+
+//==============================================================================
+// This creates new instances of the plugin..
+juce::AudioProcessor* JUCE_CALLTYPE createPluginFilter()
+{
+    return new FirstTestPluginAudioProcessor();
+}
+
+
+float FirstTestPluginAudioProcessor::hardClip (float x){
+    float y;
+    if (x > 0.5f) {
+        y = 0.5f;
+    } else if (x< -0.5f ){
+        y = -0.5f;
+    } else {
+        y = x;
+    }
+    return y;
+}
